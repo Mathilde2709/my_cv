eaa808151bab6999f708aa54a256e1ff849387d5
Mathilde Meunier <mathilde.meunier@etu.univ-grenoble-alpes.fr>
2019-01-28 11:10:20 +0000

    fin cours 1

--- my_cv/src/Controller/MathildeController.php
+++ my_cv/src/Controller/MathildeController.php
@@ -6,13 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MathildeController extends Controller
 {
-    public function number()
-    {
+        public function number($name,$namefamily)
+        {
         $number = random_int(0,100);
         
-        return $this->render('mycv/number.html.twig', array(
+        
+        return $this->render('mycv/number.html.twig',[
             'number' => $number,
-            ));
+            'name' => $name,
+            'namefamily' => $namefamily,
+            ]);
     }
 }
+
+
 
